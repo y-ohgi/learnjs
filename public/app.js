45e21a8a4918650c201bea006060951e6db71f60
y-ohgi <ohgi.211@gmail.com>
2018-02-03 17:28:05 +0900

    add 2.4

--- public/app.js
+++ public/app.js
@@ -1,6 +1,18 @@
 'use strict';
 var learnjs = {};
+
+learnjs.problemView = function(problemNumber) {
+  var title = 'Problem #' + problemNumber +  ' Coming soon!';
+  return $('<div class="problem-view">').text(title);
+}
+
 learnjs.showView = function(hash) {
-  var problemView = $('<div class="problem-view">').text('Comming soon!');
-  $('.view-container').empty().append(problemView)
+  var routes = {
+    '#problem': learnjs.problemView
+  };
+  var hashParts = hash.split('-')
+  var viewFn = routes[hashParts[0]];
+  if(viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]))
+  }
 }
