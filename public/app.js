6f137dd597c42f1e0e98c5b6794d07ea27ccec8b
y-ohgi <ohgi.211@gmail.com>
2018-02-04 15:32:26 +0900

    add 3.3.1

--- public/app.js
+++ public/app.js
@@ -3,22 +3,71 @@ var learnjs = {};
 
 learnjs.appOnReady = function() {
   window.onhashchange = function() {
-    learnjs.showView(window.location.hash)
+    learnjs.showView(window.location.hash);
   };
-}
-
-learnjs.problemView = function(problemNumber) {
-  var title = 'Problem #' + problemNumber +  ' Coming soon!';
-  return $('<div class="problem-view">').text(title);
+  learnjs.showView(window.location.hash);
 }
 
 learnjs.showView = function(hash) {
   var routes = {
     '#problem': learnjs.problemView
   };
-  var hashParts = hash.split('-')
+  var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
   if(viewFn) {
-    $('.view-container').empty().append(viewFn(hashParts[1]))
+    $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
+
+learnjs.problemView = function(data) {
+  var problemNumber = parseInt(data, 10);
+  var view = $('.templates .problem-view').clone();
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+
+  function checkAnswer() {
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('__', answer) + '; problem();';
+    return eval(test);
+  }
+
+  function checkAnswerClick() {
+    if(checkAnswer()) {
+      learnjs.flashElement(resultFlash, 'Correct!')
+    } else {
+      learnjs.flashElement(resultFlash, 'Incorrect!')
+    }
+
+    return false;
+  }
+
+  view.find('.check-btn').click(checkAnswerClick);
+  view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(problemData, view);
+  return view;
+}
+
+learnjs.flashElement = function(elem, content) {
+  elem.fadeOut('fast', function() {
+    elem.html(content);
+    elem.fadeIn();
+  });
+}
+
+learnjs.applyObject = function(obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
+
+
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  }
+];
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -15,18 +15,45 @@ describe('LearnJS', function() {
     expect(learnjs.problemView).toHaveBeenCalledWith('42')
   });
 
-  describe('problem view', function() {
-    it('has a title that includes the problem number', function() {
-      var view = learnjs.problemView('1');
-      expect(view.text()).toEqual('Problem #1 Coming soon!')
-    });
-  });
-
   it('subscribes to the hash change event', function() {
     learnjs.appOnReady();
     spyOn(learnjs, 'showView');
     $(window).trigger('hashchange');
     expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
   });
-});
+  
+  describe('problem view', function() {
+    it('has a title that includes the problem number', function() {
+      var view = learnjs.problemView('1');
+      expect(view.find('.title').text()).toEqual('Problem #1');
+    });
+    
+    it('show the description', () => {
+      var view = learnjs.problemView('1');
+      expect(view.find('[data-name="description"]').text()).toEqual('What is truth?')
+    });
 
+    it('shows the problem code', function() {
+      var view = learnjs.problemView('1');
+      expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }')
+    });
+  });
+
+  describe('answer section', function() {
+    
+    it('can check a correct answer by hitting a button', function() {
+      var view = learnjs.problemView('1');
+      view.find('.answer').val('true')
+      view.find('.check-btn').click()
+      expect(view.find('.result').text()).toEqual('Correct!')
+    });
+
+    it('reject an incorrect answer', function() {
+      var view = learnjs.problemView('1');
+      view.find('.answer').val('false')
+      view.find('.check-btn').click()
+      expect(view.find('.result').text()).toEqual('Incorrect!')
+    })
+    
+  })
+});
